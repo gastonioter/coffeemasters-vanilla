440f8396ae60327658e196b4aee7210d26f13053
gastonioter <iotting.castro10@gmail.com>
2024-07-25 03:46:55 -0300

    refactor and 404 page

--- services/Menu.js
+++ services/Menu.js
@@ -5,19 +5,18 @@ export async function loadMenu() {
   // this set triggers an event in the store
   Store.menu = await API.fetchMenu();
 }
-
 export async function loadProductById(id) {
   if (!Store.menu) {
     await loadMenu();
   }
 
-  for (let category of Store.menu) {
-    for (let product of category.products) {
-      if (product.id == id) {
-        return product;
-      }
-    }
-  }
+  let alls = [];
 
-  return null;
+  Store.menu.forEach(function collect({ products } = {}) {
+    alls.push(...products);
+  });
+
+  return alls.find(function matchId(prod) {
+    return prod.id == id;
+  });
 }
